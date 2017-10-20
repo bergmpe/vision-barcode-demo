27bd2fc5ba6f699040f6553f09d59ca584bb75c6
bergmpe <bergmpe@gmail.com>
2017-10-20 11:19:57 -0200

    Initial Commit

---
+++ BarCode Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BarCode Demo
+//
+//  Created by padrao on 20/10/17.
+//  Copyright © 2017 padrao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
